52c491c0906e141e959769996e4b00451b31461d
RickySulli <71475646+RickySulli@users.noreply.github.com>
2020-11-21 13:57:33 -0600

    missing badge still

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./util/generate-markdown')
+const generateMarkdown = require('./util/generate-markdown');
 
 // License function and  if/else section here 
-let getLicence = value => {   
-     if (value === "GNU LGPLv3") {
+let getLicense = param => {   
+     if (param === "GNU LGPLv3") {
         return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
-    }  else if (value === "GNU AGPLv3") {
+    }  else if (param === "GNU AGPLv3") {
         return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)"
-    }  else if (value === "Apache 2.0") {
+    }  else if (param === "Apache 2.0") {
         return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-    }  else if (value === "MIT") {
+    }  else if (param === "MIT") {
         return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-    }  else if (value === "ISC") {
+    }  else if (param === "ISC") {
         return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
-    }  else if (value === "Mozilla") {
+    }  else if (param === "Mozilla") {
         return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
-    }  else if (value === "Boost Software 1.0") {
+    }  else if (param === "Boost Software 1.0") {
         return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
     }  else {
         return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
@@ -83,6 +83,7 @@ const questions = [
             "The Unlicence",
         ],
         validate: validateInput,
+        
     },
     {
         type: 'input',
@@ -113,6 +114,7 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
+    
     fs.writeFile(fileName, generateMarkdown(data), (err) => {
         if (err){
             return console.log(err);
