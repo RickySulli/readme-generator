2801968a8ecafff96f1712f13bf88620c2b58907
RickySulli <71475646+RickySulli@users.noreply.github.com>
2020-11-21 03:28:09 -0600

    undefined license

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./util/generate-markdown')
 
-
-
+// License function and  if/else section here 
+let getLicence = value => {   
+     if (value === "GNU LGPLv3") {
+        return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
+    }  else if (value === "GNU AGPLv3") {
+        return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)"
+    }  else if (value === "Apache 2.0") {
+        return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    }  else if (value === "MIT") {
+        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    }  else if (value === "ISC") {
+        return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+    }  else if (value === "Mozilla") {
+        return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+    }  else if (value === "Boost Software 1.0") {
+        return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+    }  else {
+        return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
+    }
+}
 //make functions for validate to eliminate repetative if statements
 let validateInput = value =>{
     if (value != "") {
@@ -12,8 +30,8 @@ let validateInput = value =>{
         return "Please answer the question.";
     }
 }
-let validateEmail = mail =>{
-    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(myForm.emailAddr.value))
+let validateEmail = value =>{
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value))
         {return true
         }else{
             return "This email address is invalid!";
@@ -68,9 +86,14 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'Contribution',
+        name: 'contribution',
         message: 'How can other users contribute to your project?',
         validate: validateInput
+    },
+    {   type: 'input',
+        name: 'tests',
+        message: 'Please provide any testing information.',
+        validate: validateInput,
     },
     {
         type: 'input',
@@ -90,7 +113,7 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, generateMarkdown(data), function(err){
+    fs.writeFile(fileName, generateMarkdown(data), (err) => {
         if (err){
             return console.log(err);
         }
@@ -98,14 +121,12 @@ function writeToFile(fileName, data) {
 }
 
 // function to initialize program
-function init() {
-    inquirer.prompt(questions).then((data =>{
-        console.log(JSON.stringify(data, null, ""));
-        data.getLicense = getLicence(data.license);
-        writeToFile("./example/README.md", data);
-    }));
+const init = () => inquirer.prompt(questions)
 
-}
+
 
 // function call to initialize program
-init();
+init()
+    .then((data) => writeToFile("./dist/README.md", data))
+    .then(() => console.log("CREATED!!"))
+    .catch((err)=> console.log(err));
